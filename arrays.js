351f26edc62246ad3b8ebb0a7f649195c057749f
Tom Xu <tomxu89@gmail.com>
2018-01-13 01:02:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray (arr, toAdd) {
+  var newArray = [toAdd, ...arr]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray (arr, toAdd) {
+  arr.push(toAdd)
+  return arr
+}
